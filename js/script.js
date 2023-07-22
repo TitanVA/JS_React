0ace9327854db88b6cb159f8d817a87780516ded
Viktor <viktorbezai@gmail.com>
2023-07-22 09:09:21 -0400

    64

--- js/script.js
+++ js/script.js
@@ -1,50 +1,39 @@
-/* Задания на урок:
+window.addEventListener("DOMContentLoaded", () => {
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+    const tabs = document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent"),
+        tabsParent = document.querySelector(".tabheader__items");
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add("hide");
+            item.classList.remove("show", "fade")
+        });
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add("show", "fade")
+        tabsContent[i].classList.add("hide")
+        tabs[i].classList.add("tabheader__item_active");
+    }
 
-5) Добавить нумерацию выведенных фильмов */
+    hideTabContent();
+    showTabContent(0);
 
-'use strict';
+    tabsParent.addEventListener("click", (event) => {
+        const target = event.target;
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-
-const adv = document.querySelectorAll(".promo__adv img"),
-      poster = document.querySelector(".promo__bg"),
-      genre = poster.querySelector(".promo__genre"),
-      movieList = document.querySelector(".promo__interactive-list")
-
-adv.forEach(item => {
-   item.remove();
+        if (target && target.classList.contains(".tabheader__item")) {
+            tabs.forEach((item, i) => {
+                if (target === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 });
-
-genre.textContent = "Драма";
-
-poster.style.backgroundImage = "url('img/bg.jpg')";
-
-movieList.innerHTML = "";
-
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-})
